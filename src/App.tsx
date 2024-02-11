7c17063af199384935baccbdd5e9597bdc258fef
mart1057 <81488092+mart1057@users.noreply.github.com>
2024-02-12 01:35:39 +0700

    'update'

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,9 @@ import { useRoutes } from 'react-router-dom';
 import MenuBar from "./components/ui/menuBar.tsx";
 import ContentPage from './page/ContentPages/Content.tsx';
 import SignIn from './page/SignInPage/SignIn.tsx';
-import useBearStore from '../store.ts'
-import { useState } from 'react'
- function BearCounter() {
-    const bears = useBearStore((state:any) => state.bears)
-    return <h1>{bears} around here ...</h1>
-  }
-  
-  function Controls() {
-    const [count, setCount] = useState(0)
-    const increasePopulation = useBearStore((state:any) => state.increasePopulation)
-    return <button onClick={increasePopulation}>one up</button>
-  }
+
 
 function App() {
- 
   let element = useRoutes([
     {
       path: "/",
@@ -31,8 +19,6 @@ function App() {
   return (
     <div className='mb-[24px]'>
       <MenuBar/>
-      <Controls/>
-      <BearCounter/>
       {element}
     </div>
   )
